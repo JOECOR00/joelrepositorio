4ff91762a3ea6f7c015b9cd1bcbbafa648c1deeb
joelo <joeloclase@gmail.com>
2020-10-07 09:27:14 +0200

    Nous fitxers

---
+++ index.php
@@ -0,0 +1,5 @@
+echo 'Al gener, tanca la porta i encén el braser',
+echo 'Aigua de gener tot l'any va bé',
+echo 'Pel febrer, carnestoltes el primer',
+echo 'Pel febrer flor a l'ametler',
+echo 'Mes tard o més matiner carnaval dins el febrer',
